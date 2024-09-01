4ccdeaf9e6fc30b19bc0ed7bc0e1e512c8a82737
kaiying <kaiying1991@hotmail.com>
2024-09-01 21:26:09 +0800

    update

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,48 +4,30 @@ const app = createApp({
 	data () {
 		return {
 			contributionRates: [
-				{
-					minAge: 1,
-					maxAge: 55,
-					employer: 17,
-					employee: 20
-				},
-				{
-					minAge: 56,
-					maxAge: 60,
-					employer: 15.5,
-					employee: 17
-				},
-				{
-					minAge: 61,
-					maxAge: 65,
-					employer: 12,
-					employee: 11.5
-				},
-				{
-					minAge: 66,
-					maxAge: 70,
-					employer: 9,
-					employee: 7.5
-				},
-				{
-					minAge: 71,
-					maxAge: 200,
-					employer: 7.5,
-					employee: 5
-				}
+				{ minAge: 1, maxAge: 55, employer: 17, employee: 20 },
+				{ minAge: 56, maxAge: 60, employer: 15.5, employee: 17 },
+				{ minAge: 61, maxAge: 65, employer: 12, employee: 11.5 },
+				{ minAge: 66, maxAge: 70, employer: 9, employee: 7.5 },
+				{ minAge: 71, maxAge: 200, employer: 7.5, employee: 5 }
+			],
+			allocations: [
+				{ minAge: 1, maxAge: 35, oaAllocation: 0.6217, saAllocation: 0.1621, maAllocation: 0.2162 },
+				{ minAge: 36, maxAge: 45, oaAllocation: 0.5677, saAllocation: 0.1891, maAllocation: 0.2432 },
+				{ minAge: 46, maxAge: 50, oaAllocation: 0.5136, saAllocation: 0.2162, maAllocation: 0.2702 },
+				{ minAge: 51, maxAge: 55, oaAllocation: 0.4055, saAllocation: 0.3108, maAllocation: 0.2837 },
+				{ minAge: 56, maxAge: 60, oaAllocation: 0.3872, saAllocation: 0.2741, maAllocation: 0.3387 },
+				{ minAge: 61, maxAge: 65, oaAllocation: 0.1592, saAllocation: 0.3636, maAllocation: 0.4772 },
+				{ minAge: 66, maxAge: 70, oaAllocation: 0.0607, saAllocation: 0.303, maAllocation: 0.6363 },
+				{ minAge: 71, maxAge: 200, oaAllocation: 0.08, saAllocation: 0.08, maAllocation: 0.84 }
 			],
 			age: null,
 			monthlyIncome: null,
 			contributionRate: null,
-			computedValues: {},
+			allocation: null,
+			totalContribution: null,
 
-			personalInfo: {
-				employerContribution: 0,
-				employeeContribution: 0,
-				totalContribution: 0,
-				takeHomePay: 0
-			}
+			basicResults: {},
+			allocationResults: {},
 		};
 	},
 	methods: {
@@ -57,18 +39,32 @@ const app = createApp({
 
 			let firstContribution = self.contributionRates[0];
 			let lastContribution = self.contributionRates[self.contributionRates.length - 1];
-			if (this.age < firstContribution.minAge || this.age > lastContribution.maxAge) {
-				return;
+			if (self.age < firstContribution.minAge || self.age > lastContribution.maxAge) {
+				self.contributionRate = null;
+			} else {
+				for (let i = 0; i < self.contributionRates.length; i++) {
+					if (self.age >= self.contributionRates[i].minAge && self.age <= self.contributionRates[i].maxAge) {
+						self.contributionRate = self.contributionRates[i];
+						break;
+					}
+				}
 			}
 
-			for (let i = 0; i < self.contributionRates.length; i++) {
-				if (self.age >= self.contributionRates[i].minAge && self.age <= self.contributionRates[i].maxAge) {
-					self.contributionRate = self.contributionRates[i];
-					break;
+			let firstAllocation = self.allocations[0];
+			let lastAllocation = self.allocations[self.allocations.length - 1];
+			if (self.age < firstAllocation.minAge || self.age > lastAllocation.maxAge) {
+				self.allocation = null;
+			} else {
+				for (let i = 0; i < self.allocations.length; i++) {
+					if (self.age >= self.allocations[i].minAge && self.age <= self.allocations[i].maxAge) {
+						self.allocation = self.allocations[i];
+						break;
+					}
 				}
 			}
 
 			self.calculateContribution();
+			self.calculateAllocation();
 		},
 		calculateContribution () {
 			let self = this;
@@ -86,13 +82,45 @@ const app = createApp({
 
 			let employerContribution = (contributionRate.employer / 100) * monthlyIncome;
 			let employeeContribution = (contributionRate.employee / 100) * monthlyIncome;
-			let totalContribution = employerContribution + employeeContribution;
-			let takeHomePay = monthlyIncome - totalContribution;
+			self.totalContribution = employerContribution + employeeContribution;
 
-			self.computedValues['Employer contribution'] = { 'value': '$ ' + employerContribution.toLocaleString('en-US', { minimumFractionDigits: 2, maximumFractionDigits: 2 }) + ' (' + contributionRate.employer + '%)' };
-			self.computedValues['Employee contribution'] = { 'value': '$ ' + employeeContribution.toLocaleString('en-US', { minimumFractionDigits: 2, maximumFractionDigits: 2 }) + ' (' + contributionRate.employee + '%)' };
-			self.computedValues['Total contribution'] = { 'value': '$ ' + totalContribution.toLocaleString('en-US', { minimumFractionDigits: 2, maximumFractionDigits: 2 }) };
-			self.computedValues['Take home pay'] = { 'value': '$ ' + takeHomePay.toLocaleString('en-US', { minimumFractionDigits: 2, maximumFractionDigits: 2 }) };
+			let takeHomePay = monthlyIncome - self.totalContribution;
+
+			self.basicResults['Employer contribution'] = { 'value': self.toCurrency(employerContribution) };
+			self.basicResults['Employee contribution'] = { 'value': self.toCurrency(employeeContribution) };
+			self.basicResults['Take home pay'] = { 'value': self.toCurrency(takeHomePay) };
+		},
+		calculateAllocation () {
+			let self = this;
+
+			let allocation = self.allocation;
+			if (allocation == null) {
+				return;
+			}
+
+			if (self.monthlyIncome == null) {
+				return;
+			}
+
+			if (self.totalContribution == null) {
+				return;
+			}
+
+			let saAmt = allocation.saAllocation * self.totalContribution;
+			let maAmt = allocation.maAllocation * self.totalContribution;
+			let oaAmt = self.totalContribution - saAmt - maAmt;
+
+
+			self.allocationResults['OA Allocation'] = { 'value': self.toCurrency(oaAmt) };
+			self.allocationResults['SA Allocation'] = { 'value': self.toCurrency(saAmt) };
+			self.allocationResults['MA Allocation'] = { 'value': self.toCurrency(maAmt) };
+			self.allocationResults['Total Contribution'] = { 'value': self.toCurrency(self.totalContribution) };
+		},
+		toCurrency (value) {
+			if (value == null) {
+				return value;
+			}
+			return '$ ' + value.toLocaleString('en-US', { minimumFractionDigits: 2, maximumFractionDigits: 2 });
 		}
 	},
 	mounted () {
